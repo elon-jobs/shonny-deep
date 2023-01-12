4344e060fbd303116c428358bbd21de1f6562dd5
Elon Jobs <elon.jobs.dev@gmail.com>
2023-01-11 19:51:52 -0600

    Improvements

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 # Initialize driver
 options = webdriver.ChromeOptions()
@@ -30,11 +31,14 @@ for link in links:
         if (a == 'Enlaces'):
             to_download.append(anchor.get_attribute('href'))
             
-    if(len(to_download) > 0):
-        for td in to_download:
-            driver.get(td)
-            # We will wait for the "Click aquí" button
-            print('Going to Ad site')
+    if(len(to_download) == 0):
+        print('Failed:' + link)
+        continue
+    for td in to_download:
+        print('Going to Ad site')
+        driver.get(td)
+        # We will wait for the "Click aquí" button
+        try:        
             ad_site = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'button')))
             # Once we have the presence of the button in the DOM, let's execute the noobBypass function in the Chrome console
             if (ad_site):
@@ -44,6 +48,12 @@ for link in links:
                 button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="tab1"]/form/input[5]')))
                 print('Adding links to JDownloader...')
                 button.click()
+        except TimeoutException:
+            print('Failed Byppasing: '+ td)
+            continue
+        except NoSuchElementException:
+            print('Failed Byppasing: '+ td)
+            continue       
             
 
 #Closing chromedriver        
